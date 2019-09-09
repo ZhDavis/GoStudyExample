5ae6c28a0150b1aff480936241cb1e0b89b7ecd5
ZhDavis <zhang_hui_it@163.com>
2019-09-09 09:56:16 +0800

    channel example

---
+++ channel/bufchannel.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func readblock() {
+	fmt.Println("Block Start!")
+	c1 := make(chan int,3) // c1 的容量是3，当写入的数据大于3的时候，写协程就会被阻塞住
+
+	go func() { // 1.goroutine 用来往channel写数据，写的数据要多于3个
+		for i := 0; i < 5; i++ {
+			c1 <- i
+			fmt.Println("Write data into channel:", i, time.Now())
+	        	time.Sleep(2*time.Second)
+		}
+	}()
+
+	// 2. 主 goroutine 用来从channel读数据，读数据读的比较慢
+	for i := 0; i < 5; i++ {
+		tmp := <-c1
+		fmt.Println("Read data from channel:", tmp, time.Now())
+	}
+
+	time.Sleep(2*time.Second)
+	fmt.Println("Block exit!")
+}
+func writeblock() {
+	fmt.Println("Block Start!")
+	c1 := make(chan int,3) // c1 的容量是3，当写入的数据大于3的时候，写协程就会被阻塞住
+
+	go func() { // 1.goroutine 用来往channel写数据，写的数据要多于3个
+		for i := 0; i < 5; i++ {
+			c1 <- i
+			fmt.Println("Write data into channel:", i, time.Now())
+		}
+	}()
+
+	// 2. 主 goroutine 用来从channel读数据，读数据读的比较慢
+	for i := 0; i < 5; i++ {
+		tmp := <-c1
+		fmt.Println("Read data from channel:", tmp, time.Now())
+	        time.Sleep(2*time.Second)
+	}
+
+	time.Sleep(2*time.Second)
+	fmt.Println("Block exit!")
+}
+
+func noblock() {
+	fmt.Println("Main Start!")
+	c1 := make(chan int,10)
+
+	go func() { // 1.goroutine 用来往channel写数据
+		for i := 0; i < 5; i++ {
+			c1 <- i
+			fmt.Println("Write data into channel:", i, time.Now())
+		}
+	}()
+
+	// 2. 主 goroutine 用来从channel读数据
+	for i := 0; i < 5; i++ {
+		tmp := <-c1
+		fmt.Println("Read data from channel:", tmp, time.Now())
+	}
+
+	time.Sleep(2*time.Second)
+	fmt.Println("Main exit!")
+}
+
+func main() {
+	readblock()
+	return
+}
---
+++ channel/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+func ChannelCache() {
+	fmt.Println("ChannelCache: start!")
+	defer fmt.Println("ChannelCache: exit!")
+	c := make(chan int,10)
+	defer close(c)
+	go func() {
+		c<- 2+3
+	}()
+	i := <-c
+	fmt.Println("i:",i)
+	return
+}
+
+func ChannelNoCache() {
+	fmt.Println("ChannelNoCache: start!")
+	defer fmt.Println("ChannelNoCache: exit!")
+	c := make(chan int)
+	defer close(c)
+	go func() {
+		c<- 2+3
+	}()
+	i := <-c
+	fmt.Println("i:",i)
+	return
+}
+
+func main() {
+	fmt.Println("Main start!")
+	defer fmt.Println("Main exit!")
+
+	ChannelNoCache()
+	ChannelCache()
+	return
+}
